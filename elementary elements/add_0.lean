370611414bd8772d3a6654ae792debd7c715fa59
Taka <103984069+Taka0007@users.noreply.github.com>
2023-12-09 06:43:02 +0900

    Update add_0.lean

--- elementary elements/add_0.lean
+++ elementary elements/add_0.lean
@@ -1,4 +1,5 @@
 variable x : ℕ  -- 自然数の変数xを定義
 def add (a b : ℕ) : ℕ := a + b  -- 二つの自然数を加算する関数
+-- 自然数aに0を加えるとaになることの証明
 theorem add_zero (a : ℕ) : add a 0 = a := 
-  rfl  -- 自然数aに0を加えるとaになることの証明
+  rfl
