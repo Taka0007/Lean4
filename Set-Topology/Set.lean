9f7b48ce1751c369be3efc942006be1c784329c0
Taka <103984069+Taka0007@users.noreply.github.com>
2023-12-09 09:26:21 +0900

    add Proof
    f(x) = x+1 が単射であることの証明

--- Set-Topology/Set.lean
+++ Set-Topology/Set.lean
@@ -1,3 +1,6 @@
+-- import
+import Mathlib.Tactic
+
 -- 集合X,Y,Zを定義
 variable {X Y Z : Type}
 
@@ -6,3 +9,8 @@ f(a)=f(b)ならばa=bが成立
 -/
 def Injective (f : X → Y) : Prop :=
   ∀ {x₁ x₂ : X}, f x₁ = f x₂ → x₁ = x₂ 
+
+-- f(x) = x+1 が単射であることの証明
+example : Injective (fun x : ℕ ↦ x + 1) := by
+  rw [Injective]
+  simp
