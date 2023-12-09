5bfdf6f6c8ee7ffc970975971ae6c84901ed6190
Taka <103984069+Taka0007@users.noreply.github.com>
2023-12-09 09:37:42 +0900

    add proof
    f(x) = x+N (Nは自然数) が単射であることの証明

--- Set-Topology/Set.lean
+++ Set-Topology/Set.lean
@@ -4,6 +4,10 @@ import Mathlib.Tactic
 -- 集合X,Y,Zを定義
 variable {X Y Z : Type}
 
+-- 自然数N,実数Rを定義
+variable {N : ℕ}
+variable {Q : ℚ}
+
 /- ## 単射の定義
 f(a)=f(b)ならばa=bが成立
 -/
@@ -12,5 +16,11 @@ def Injective (f : X → Y) : Prop :=
 
 -- f(x) = x+1 が単射であることの証明
 example : Injective (fun x : ℕ ↦ x + 1) := by
+  rw [Injective]
+  simp
+
+
+-- f(x) = x+N (Nは自然数) が単射であることの証明
+example : Injective (fun x : ℕ ↦ x + N) := by
   rw [Injective]
   simp
