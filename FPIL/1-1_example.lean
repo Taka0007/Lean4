78c1d9cdf4e136d5b8f8e7a7966588ce811bd108
Taka <103984069+Taka0007@users.noreply.github.com>
2024-01-30 01:00:29 +0900

    Update 1-1_example.lean

--- FPIL/1-1_example.lean
+++ FPIL/1-1_example.lean
@@ -7,13 +7,13 @@
 
 
 -- 下記のように、leanのevalは一般的な計算規則に従う
-
 #eval 1 + 2 * 5
 -- output
 -- 11
 
+
+-- 文字列の結合を行うことも可能だが、これはエラーになる (下記がエラーメッセージ)
 #eval String.append("Hello, ", "Lean!")
--- これはエラーになる (下記がエラーメッセージ)
 -- expression
 -- String.append
 -- has type
@@ -23,6 +23,12 @@
 -- failed to be synthesized, this instance instructs Lean on how to display the resulting value, recall that any type implementing the `Repr` class also implements the `Lean.Eval` class
 
 
+-- このように変えれば動く
 #eval String.append "Hello, " "Lean!"
 -- output
 -- "Hello, Lean!"
+
+
+-- 結合させたい文字を最初に持ってくることも可能
+#eval String.append "great " (String.append "oak " "tree")
+-- "great oak tree"
