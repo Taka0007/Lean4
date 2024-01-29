759b6f103ad2998588902a944a3d0afd2135e472
Taka <103984069+Taka0007@users.noreply.github.com>
2024-01-30 00:57:59 +0900

    Update 1-1_example.lean

--- FPIL/1-1_example.lean
+++ FPIL/1-1_example.lean
@@ -11,3 +11,18 @@
 #eval 1 + 2 * 5
 -- output
 -- 11
+
+#eval String.append("Hello, ", "Lean!")
+-- これはエラーになる (下記がエラーメッセージ)
+-- expression
+-- String.append
+-- has type
+--   String → String → String
+-- but instance
+--   Lean.Eval (String → String → String)
+-- failed to be synthesized, this instance instructs Lean on how to display the resulting value, recall that any type implementing the `Repr` class also implements the `Lean.Eval` class
+
+
+#eval String.append "Hello, " "Lean!"
+-- output
+-- "Hello, Lean!"
