d73b264fd48b9d37e38ec0259694588b28dd7e36
Taka <103984069+Taka0007@users.noreply.github.com>
2024-04-12 11:47:43 +0900

    Update S02.lean

--- MIL/C02/S02.lean
+++ MIL/C02/S02.lean
@@ -3,6 +3,36 @@ import Mathlib.Algebra.Ring.Defs
 import Mathlib.Data.Real.Basic
 
 
+variable (R : Type*) [Ring R]
+
+#check (add_assoc : ∀ a b c : R, a + b + c = a + (b + c))
+-- add_assoc : ∀ (a b c : R), a + b + c = a + (b + c)
+
+#check (add_comm : ∀ a b : R, a + b = b + a)
+-- add_comm : ∀ (a b : R), a + b = b + a
+
+#check (zero_add : ∀ a : R, 0 + a = a)
+-- ero_add : ∀ (a : R), 0 + a = a
+
+#check (add_left_neg : ∀ a : R, -a + a = 0)
+-- add_left_neg : ∀ (a : R), -a + a = 0
+
+#check (mul_assoc : ∀ a b c : R, a * b * c = a * (b * c))
+-- mul_assoc : ∀ (a b c : R), a * b * c = a * (b * c)
+
+#check (mul_one : ∀ a : R, a * 1 = a)
+-- mul_one : ∀ (a : R), a * 1 = a
+
+#check (one_mul : ∀ a : R, 1 * a = a)
+-- one_mul : ∀ (a : R), 1 * a = a
+
+#check (mul_add : ∀ a b c : R, a * (b + c) = a * b + a * c)
+-- mul_add : ∀ (a b c : R), a * (b + c) = a * b + a * c
+
+#check (add_mul : ∀ a b c : R, (a + b) * c = a * c + b * c)
+-- add_mul : ∀ (a b c : R), (a + b) * c = a * c + b * c
+
+
 theorem add_neg_cancel_right (a b : R) : a + b + -b = a := by
   sorry
 
