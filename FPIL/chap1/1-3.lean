8b0e7a1cb2442c6d329294fcb6139dd55914b8e3
Taka <103984069+Taka0007@users.noreply.github.com>
2024-04-15 13:54:49 +0900

    関数の定義
    文字列を表す関数の定義について

--- FPIL/chap1/1-3.lean
+++ FPIL/chap1/1-3.lean
@@ -1,3 +1,11 @@
 -- 関数の定義
 -- 新たな名前を定義する際には「:=」を使用する
 def hello := "Hello"
+
+-- 通常は関数の定義の際には型も併せて定義
+def lean : String := "Lean"
+-- 関数の使用例
+#eval String.append hello (String.append " " lean)
+-- output
+-- "Hello Lean"
+
