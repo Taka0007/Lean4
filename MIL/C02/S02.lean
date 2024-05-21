9a5af46ae3f33c0067052335f767dc45e11f6f32
Taka <103984069+Taka0007@users.noreply.github.com>
2024-05-21 16:52:49 +0900

    Solved add_neg_cancel_right_1

--- MIL/C02/S02.lean
+++ MIL/C02/S02.lean
@@ -34,13 +34,21 @@ variable (R : Type*) [Ring R]
 
 
 -- Exercise
-theorem add_neg_cancel_right (a b : R) : a + b + -b = a := by
+
+-- matlib使用ver
+theorem add_neg_cancel_right_ (a b : R) : a + b + -b = a := by
+  apply add_neg_cancel_right  -- No goals
+
+-- mathlib未使用ver
+theorem add_neg_cancel_right_1 (a b : R) : a + b + -b = a := by  --  a + b + -b = a
+  rw [add_assoc a b (-b)]  -- a + (b + -b) = a
+  rw [add_right_neg b]     -- a + 0 = a
+  rw [add_zero a]          -- No goals
+
+theorem add_left_cancel_ {a b c : R} (h : a + b = a + c) : b = c := by
   sorry
 
-theorem add_left_cancel {a b c : R} (h : a + b = a + c) : b = c := by
-  sorry
-
-theorem add_right_cancel {a b c : R} (h : a + b = c + b) : a = c := by
+theorem add_right_cancel_ {a b c : R} (h : a + b = c + b) : a = c := by
   sorry
 
 theorem zero_mul (a : R) : 0 * a = 0 := by
@@ -52,15 +60,15 @@ theorem neg_eq_of_add_eq_zero {a b : R} (h : a + b = 0) : -a = b := by
 theorem eq_neg_of_add_eq_zero {a b : R} (h : a + b = 0) : a = -b := by
   sorry
 
-theorem neg_zero : (-0 : R) = 0 := by
+theorem neg_zero_ : (-0 : R) = 0 := by
   apply neg_eq_of_add_eq_zero
   rw [add_zero]
 
-theorem neg_neg (a : R) : - -a = a := by
+theorem neg_neg_ (a : R) : - -a = a := by
   sorry
 
-theorem self_sub (a : R) : a - a = 0 := by
+theorem self_sub_ (a : R) : a - a = 0 := by
   sorry
 
-theorem two_mul (a : R) : 2 * a = a + a := by
+theorem two_mul_ (a : R) : 2 * a = a + a := by
   sorry
