ba87d5a9fa632cc7da92041d1684cc67f49dc42b
Taka <103984069+Taka0007@users.noreply.github.com>
2024-05-29 14:25:39 +0900

    Update Ex001.lean

--- Lean99/code/Ex001.lean
+++ Lean99/code/Ex001.lean
@@ -1,1 +1,14 @@
+import Lean
 
+def lastOfList {α : Type} (l : List α) : Option α :=
+  match l with
+  | [] => none
+  | [x] => some x
+  | _ :: xs => lastOfList xs
+
+def exampleList : List Nat := [1, 2, 3, 4, 5]
+def exampleList2 : List Nat := [1,2,3]
+
+#eval lastOfList exampleList  -- some 5
+#eval lastOfList exampleList2 -- some 3
+#eval lastOfList ([] : List Nat)  -- none
