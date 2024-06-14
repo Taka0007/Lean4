c06c3c7de0919352ee9e1fc8e5e7c981947443d6
Taka <103984069+Taka0007@users.noreply.github.com>
2024-06-15 02:18:50 +0900

    Update Ex002.lean

--- Lean99/code/Ex002.lean
+++ Lean99/code/Ex002.lean
@@ -1,1 +1,18 @@
+-- import
+import Lean
 
+-- define function
+def myButLast (l : List α) : Option α :=
+  if l.length <= 1 then
+    none
+  else
+    let num:= l.length - 2;
+    l[num]?
+
+-- test
+def exampleList : List Nat := [1, 2, 3, 4, 5]
+def exampleList2 : List Nat := [1, 2, 3]
+
+#eval myButLast exampleList  -- some 4
+#eval myButLast exampleList2 -- some 2
+#eval myButLast ([] : List Nat)  -- none
