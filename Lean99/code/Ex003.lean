4e99164d522e9ed8aae8ee838afbcd017edcd61f
Taka <103984069+Taka0007@users.noreply.github.com>
2024-06-15 02:28:36 +0900

    Add test cases

--- Lean99/code/Ex003.lean
+++ Lean99/code/Ex003.lean
@@ -5,6 +5,8 @@ import Lean
 def elementAt (l : List α) (k : Nat) : Option α :=
   if l.length <= k-1 then
     none
+  else if k = 0 then
+    none
   else
     l[k-1]?
 
@@ -17,3 +19,10 @@ def exampleList3 : List Nat := [1, 2]
 #eval elementAt exampleList2 2 -- some 2
 #eval elementAt ([] : List Nat) 3  -- none
 #eval elementAt exampleList3 56  -- none
+
+-- test cases of Lean99
+example : elementAt ['a', 'b', 'c', 'd', 'e'] 3 = some 'c' := by rfl
+example : elementAt ['a', 'b', 'c', 'd', 'e'] 6 = none := by rfl
+example : elementAt ['a', 'b', 'c', 'd', 'e'] 0 = none := by rfl
+example : elementAt ([] : List α) 0 = none := by rfl
+example : elementAt [1, 2, 3] 2 = some 2 := by rfl
