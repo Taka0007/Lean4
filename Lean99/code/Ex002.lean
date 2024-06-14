40e4d144a3c64c981825875b5b2a4804ef17dcde
Taka <103984069+Taka0007@users.noreply.github.com>
2024-06-15 02:20:18 +0900

    Add Test

--- Lean99/code/Ex002.lean
+++ Lean99/code/Ex002.lean
@@ -10,9 +10,11 @@ def myButLast (l : List α) : Option α :=
     l[num]?
 
 -- test
-def exampleList : List Nat := [1, 2, 3, 4, 5]
+def exampleList  : List Nat := [1, 2, 3, 4, 5]
 def exampleList2 : List Nat := [1, 2, 3]
+def exampleList3 : List Nat := [1, 2]
 
 #eval myButLast exampleList  -- some 4
 #eval myButLast exampleList2 -- some 2
 #eval myButLast ([] : List Nat)  -- none
+#eval myButLast exampleList3   -- some 1
